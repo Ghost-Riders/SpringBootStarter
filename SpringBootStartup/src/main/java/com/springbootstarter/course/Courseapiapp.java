1539bdb0b2a5289520400e6c7f99c2493d3d1bd7
root <root@ghostrider>
2019-11-10 20:52:25 +0530

    Spring Boot Calling main method

---
+++ SpringBootStartup/src/main/java/com/springbootstarter/course/Courseapiapp.java
@@ -0,0 +1,14 @@
+package com.springbootstarter.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Courseapiapp {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Courseapiapp.class, args);
+	}
+
+}
